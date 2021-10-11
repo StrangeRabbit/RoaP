a1501bcbd8ddce6f2dfae58fd935eb49b7cf8270
Gonçalo Frazão <gonfrazao@gmail.com>
2021-10-11 18:56:57 +0100

    projeto intermedio final version

--- main.c
+++ main.c
@@ -70,6 +70,9 @@ int main(int argc, char** argv)
     }
     fclose(ofp);
     fclose(fp);
+ 
+    
+ 
     exit(0);
 }
 
--- structures.h
+++ structures.h
@@ -6,8 +6,8 @@ typedef enum {FALSE,TRUE} BOOL;
 typedef struct
 {
     short int wall;
-    short int group;
-    short int sz;
+    unsigned short int sz;
+    unsigned int group;
 } cell;
 
 typedef enum {A1 = 1, A2, A3, A4, A5, A6} mode;
