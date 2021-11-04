900941adf4a49a63c9720586da7e6ff8fe8cf7a1
Gonçalo Frazão <gonfrazao@gmail.com>
2021-11-04 12:00:50 +0000

    updates to main

--- roap3_0/main.c
+++ roap3_0/main.c
@@ -37,50 +37,61 @@ int main(int argc, char **argv)
     while (fscanf(fp, "%d %d", &row, &column) == 2)
     {
         flag = 0;
+<<<<<<< HEAD
         get_header(fp, &game_mode, &i1, &j1, &i2, &j2, &wall_number);
+=======
+        if(project == INTERMEDIO){
+            get_header(fp, &game_mode, &i1, &j1, &i2, &j2, &wall_number);
+>>>>>>> 3560a24 (changed the way main acts to intermediate or final)
 
-        if (is_cell_in_board(row, column, i1, j1) == false)
-            flag = -2;
-        if (game_mode == A6)
-            if (is_cell_in_board(row, column, i2, j2) == false)
+            if (is_cell_in_board(row, column, i1, j1) == false)
                 flag = -2;
-        if (flag == -2)
-        {
-            jump_map(fp, wall_number);
-            fprintf(ofp, "%d\n\n", flag);
-            continue;
-        }
+            if (game_mode == A6)
+                if (is_cell_in_board(row, column, i2, j2) == false)
+                    flag = -2;
+            if (flag == -2)
+            {
+                jump_map(fp, wall_number);
+                fprintf(ofp, "%d\n\n", flag);
+                continue;
+            }
 
-        // Builds the board that will be played
-        matrix = build_board(fp, row, column, wall_number);
+            // Builds the board that will be played
+            matrix = build_board(fp, row, column, wall_number);
 
-        // Calls the game mode
-        switch (game_mode)
-        {
-        case A1:
-            mode_A1(ofp, matrix, i1, j1, row, column);
-            break;
-        case A2:
-            mode_A2(ofp, matrix, i1, j1, row, column);
-            break;
-        case A3:
-            mode_A3(ofp, matrix, i1, j1, row, column);
-            break;
-        case A4:
-            mode_A4(ofp, matrix, i1, j1, row, column);
-            break;
-        case A5:
-            mode_A5(ofp, matrix, i1, j1, row, column);
-            break;
-        case A6:
-            mode_A6(ofp, matrix, i1, j1, i2, j2, row, column);
-            break;
-        default:
-            exit(0);
-            break;
+            // Calls the game mode
+            switch (game_mode)
+            {
+            case A1:
+                mode_A1(ofp, matrix, i1, j1, row, column);
+                break;
+            case A2:
+                mode_A2(ofp, matrix, i1, j1, row, column);
+                break;
+            case A3:
+                mode_A3(ofp, matrix, i1, j1, row, column);
+                break;
+            case A4:
+                mode_A4(ofp, matrix, i1, j1, row, column);
+                break;
+            case A5:
+                mode_A5(ofp, matrix, i1, j1, row, column);
+                break;
+            case A6:
+                mode_A6(ofp, matrix, i1, j1, i2, j2, row, column);
+                break;
+            default:
+                exit(0);
+                break;
+            }
+            // Frees the board
+            free(matrix);
         }
-        // Frees the board
-        free(matrix);
+        else{
+            get_header_final(fp, &i1, &i2, &wall_number);
+            
+        }
+        
     }
     // Closes all the files
     fclose(ofp);
