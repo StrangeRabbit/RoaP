fe12e6860dbf677c5c6abe4785c07368f9cb7a74
Gonçalo Frazão <gonfrazao@gmail.com>
2021-11-07 23:27:18 +0000

    no recursives anymore

--- map.c
+++ map.c
@@ -5,36 +5,65 @@
 #define abort() printf("GOT FUCKED\n");
 #define newline() fprintf(output, "\n");
 
-int main(int argc, char **argv)
+void print_walls(int V, int C, int *dist, int *parent, int *graph, int walls, FILE *fp)
 {
-    int L, C, P, i, j, v;
-    char shit[5];
-    FILE *fp = fopen(argv[1], "r");
-    FILE *output = fopen("map.txt", "w");
-    fscanf(fp, "%d %d", &L, &C);
-    fscanf(fp, "%d %d" , &i, &j);
-    fscanf(fp, "%d", &P);
-    int **graph = (int**) calloc(L, sizeof(int*));
-    if(graph == NULL) {abort(); exit(0);} 
-    for(int a = 0; a < L; a++){
-        graph[a] = (int*) calloc(C, sizeof(int));
-        if(graph[a] == NULL) {abort(); exit(0);} 
-    }
-    for(int a = 0; a < P; a++)
-    {
-        fscanf(fp, "%d %d %d", &i, &j, &v);
-        graph[i][j] = v;
-    }
-    for(int a  = 0; a < L; a++)
-    {
-        for(int b = 0; b < C; b++)
-        {
-            fprintf(output, "%6d ", graph[a][b]);
+    int aux = parent[V] - V;
+    int v = V + aux / 2;
+    int caller = aux != 1 && aux != -1 && aux != C && aux != -C;
+    if(parent[V] != 0) {
+        if (caller){
+            print_walls(parent[V], C, dist, parent, graph, ++walls, fp);
         }
-        newline();
-        free(graph[a]);
+        else{
+            print_walls(parent[V], C, dist, parent, graph, walls, fp);
+        }
+        
     }
-    free(graph);
-    fclose(fp);
-    fclose(output);
+    if(parent[V] == 0 && V != 1 && V != C) walls++;
+    if(parent[V] == 0)
+        fprintf(fp, "%d\n", walls);
+    if(caller){
+        fprintf(fp, "%d %d %d\n", i_idx(v, C) + 1, j_idx(v, C) + 1, graph[v]);
+        //fprintf(fp, "%d %d %d -- %d\n", V, v, parent[V], aux);
+        
+    }
+    return;
+}
+
+void print_walls(int P, int src, int *walls, int *graph, FILE *fp, int C)
+{
+    int i;
+    for(i = P - 1; i >= 0; i--){
+        fprintf(fp, "%d %d %d\n", i_idx(walls[i], C) + 1, j_idx(walls[i], C) + 1, graph[walls[i]]);
+    }
+    return;
+}
+
+int count_walls(int *parent, int src, int C)
+{
+    int aux, wall = 0;
+    while(src != 0){
+        aux = parent[src] - src;
+        if(aux != 1 && aux != -1 && aux != C && aux != -C)
+            wall++;
+        src = parent[src];
+    }
+    return wall;
+}
+
+int *get_walls(int *parent, int src, int W, int C)
+{
+    int *walls = (int*) malloc(sizeof(int) * W);
+    int aux, wall = 0;
+    int i = 0;
+    while(src != 0){
+        aux = parent[src] - src;
+        if(aux != 1 && aux != -1 && aux != C && aux != -C){
+            wall = src + aux / 2;
+            walls[i] = wall;
+            i++;
+        }
+        src = parent[src];
+    }
+    return walls;
 }
