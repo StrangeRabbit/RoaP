34ed6272724d44bc36f203f5a96d9a8ea6123a68
Gonçalo Frazão <gonfrazao@gmail.com>
2021-11-09 12:55:41 +0000

    it was missing 1 include in .h

--- roap4_0/djisktra.h
+++ roap4_0/djisktra.h
@@ -1,6 +1,8 @@
 #ifndef _DJISKTRA_
 #define _DJISKTRA_
 
+#include "adjacencyList.h"
+
 void djisktra(list **graph, int room2, int* dist, int *parent, bool *sptSet, int V);
 int i_idx(int idx, int C);
 int j_idx(int idx, int C);
