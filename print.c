45deeb6d7ca7384066ec105a5d73c823edd7a550
Gonçalo Frazão <gonfrazao@gmail.com>
2021-11-11 14:56:44 +0000

    final version

--- print.c
+++ print.c
@@ -9,18 +9,18 @@
 void printWalls(_room **list, int *parent, int treasure, FILE *ofp, int C, int walls)
 {
       _room *aux = list[treasure];
-      if (parent[treasure] != treasure)
+      if (parent[treasure] != 0)
       {
             printWalls(list, parent, parent[treasure], ofp, C, ++walls);
       }
       else
       {
-            fprintf(ofp, "%d\n", walls);
+            fprintf(ofp, "%d\n", ++walls);
       }
 
       while (aux->room != parent[treasure] && aux != NULL)
             aux = aux->next;
 
-      fprintf(ofp, "%d %d %d\n", I(aux->v, C), J(aux->v, C), aux->cost);
+      fprintf(ofp, "%d %d %d\n", I(aux->v, C) + 1, J(aux->v, C) + 1, aux->cost);
       return;
 }
