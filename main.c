a68838e39f92c41197df6ed4963026a7672c73aa
Gonçalo Frazão <gonfrazao@gmail.com>
2021-11-11 15:41:42 +0000

    removed prints

--- main.c
+++ main.c
@@ -80,10 +80,10 @@ int main(int argc, char **argv)
             BFS_empty_queue(&head, &tail);
             continue;
         }
-        printf("bfs\n");
+        
         /* connect rooms */
         bfs(graph, L, C, &N_rooms);
-        printf("out\n");
+        
         /* check cost 0 */
         if (graph[0] == graph[objective])
         {
