e6e3e5f7b058b631d249574ebc5b271800aac86e
Andre-R-Teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2021-11-12 14:51:25 +0000

    Print.c commented

--- print.c
+++ print.c
@@ -6,6 +6,15 @@
 #include "lib.h"
 #include "list.h"
 
+/**
+ * @brief Recursive function to print wall destroied walls in the map
+ * @param list Edge vertice matrix where the cost and index of walls are inserted
+ * @param treasure index of the treasure in the map
+ * @param ofp Output file pointer
+ * @param C number of columns in the map
+ * @param walls number of walls already counted an ready to be insert
+ */
+
 void printWalls(_room **list, int *parent, int treasure, FILE *ofp, int C, int walls)
 {
       _room *aux = list[treasure];
