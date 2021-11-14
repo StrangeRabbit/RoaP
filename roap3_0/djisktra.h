c1da451f5c3a96605578b9b977723b37342ab956
Gonçalo Frazão <gonfrazao@gmail.com>
2021-11-14 20:39:38 +0000

    only the important

--- roap3_0/djisktra.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DJISKTRA_
-#define _DJISKTRA_
-
-void djisktra(int *graph, int L, int C, int* dist, int *parent, bool *sptSet, int src);
-int i_idx(int idx, int C);
-int j_idx(int idx, int C);
-
-#endif
